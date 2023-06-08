f426acdbdd1593b4f825ad9424b012363ef40a1c
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-08 13:14:41 +0600

    class collection get all data

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client.db('photographyDb').collection("class");
+
+
+    app.get('/class', async(req,res) => {
+        const result = await classCollection.find().toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
