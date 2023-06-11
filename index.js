18682918db466e25ab3640d851393a4e8025a9f7
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-11 23:36:25 +0600

    users admin id set admin api

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('users/admin/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id:new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role:'admin'
+        },
+      };
+      const result = await userCollection.updateOne(filter,updateDoc);
+      res.send(result);
+
+    })
+
 
     // class collection
     app.get('/class', async (req, res) => {
